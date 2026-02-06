3f6f4056b6cf1e3d4f1224f0e6425fe79a13b16b
SoberSquid <61894537+SoberSquid@users.noreply.github.com>
2026-02-06 09:48:58 -0500

    Nothing but a version update to 1.0.1 due to prior changes

--- src/main/java/com/sobersquid/ridelock/RideLock.java
+++ src/main/java/com/sobersquid/ridelock/RideLock.java
@@ -7,7 +7,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import org.lwjgl.input.Keyboard;
 
-@Mod(modid = "ridelock", name = "Ride Lock", version = "1.0")
+@Mod(modid = "ridelock", name = "Ride Lock", version = "1.0.1")
 public class RideLock {
     public static KeyBinding toggleKey;
     @Mod.EventHandler
